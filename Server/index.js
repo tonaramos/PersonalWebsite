09f7f6a8b7a129a724885fd0b996bed91b4f30a7
tonaramos <gtonaramos@gmail.com>
2018-12-05 02:46:51 -0800

    Added SendGrid to recieve web messages via email.

--- Server/index.js
+++ Server/index.js
@@ -1,13 +1,19 @@
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-const nodemailer = require('nodemailer');
-require('dotenv').config();
+const sgMail = require('@sendgrid/mail');
+
+sgMail.setApiKey(process.env.SENDGRID_KEY);
+
+
+// const nodemailer = require('nodemailer');
+// const sendgridTransport = require('nodemailer-sendgrid-transport');
+
 
 const app = express();
 
 app.use(bodyParser.json());
-
 
 // const transporter = nodemailer.createTransport(sendgridTransport({
 //   auth: {
@@ -15,27 +21,27 @@ app.use(bodyParser.json());
 //   },
 // }));
 
-
-
 app.use(express.static(path.join(__dirname, '../public')));
 
 app.post('/sendMessage', (req, res, next) => {
   console.log('The Request.Body at the Post handler-> ', req.body);
-  transporter.sendMail({
+  const msg = {
     to: 'tona@tonatiuhramos.com',
     from: req.body.email,
-    message: req.body.message,
     subject: `Message from your website - ${req.body.name}!`,
-  });
-  res.status(200).json({
-    message: 'POST request - Tempting to send a new message!',
-    body: req.body,
-  });
+    text: req.body.message,
+    html: `<p>${req.body.message}</p>`,
+  };
+  sgMail.send(msg)
+    .then(() => {
+      res.status(200).json({
+        message: 'POST request -  a new message!',
+        body: req.body,
+      });
+    }).catch((err) => {
+      console.log(err);
+    });
 });
-// app.get('/*', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../public/index.html'));
-//   console.log('Hellow from the server');
-// });
 
 const port = process.env.PORT;
 app.listen(port, () => console.log(`Listening on port >>> ${port}`));
